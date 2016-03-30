c8ae9e2270808a248da71a08b4ad19503452e10a
nh-99 <ifence9@gmail.com>
2016-03-30 15:45:09 -0400

    Posting to server backend

--- NoiseReporter/app/src/main/java/com/nohowdezign/noisereporter/NoiseMap.java
+++ NoiseReporter/app/src/main/java/com/nohowdezign/noisereporter/NoiseMap.java
@@ -2,7 +2,10 @@ package com.nohowdezign.noisereporter;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
@@ -16,12 +19,23 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.loopj.android.http.AsyncHttpClient;
 import com.nohowdezign.noisereporter.audio.SoundMeter;
+import com.nohowdezign.noisereporter.network.SendData;
+
+import org.json.JSONException;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class NoiseMap extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private SoundMeter soundMeter;
+    private List<Double> decibelMeasures = new ArrayList<Double>();
+    private List<Double> averageDecibelMeasures = new ArrayList<Double>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,14 +48,9 @@ public class NoiseMap extends FragmentActivity implements OnMapReadyCallback {
         mapFragment.getMapAsync(this);
         soundMeter = new SoundMeter();
         soundMeter.start();
-        final Handler handler = new Handler();
-        handler.post(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println(soundMeter.getDb());
-                handler.postDelayed(this, 1000);
-            }
-        });
+        storeDecibels();
+        calculateAverages();
+        sendData();
     }
 
 
@@ -58,16 +67,102 @@ public class NoiseMap extends FragmentActivity implements OnMapReadyCallback {
         mMap.moveCamera(CameraUpdateFactory.zoomTo(14));
     }
 
+    private void storeDecibels() {
+        // Thread to get the dB
+        final Handler decibelHandler = new Handler();
+        decibelHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                decibelMeasures.add(soundMeter.getDb());
+                decibelHandler.postDelayed(this, 5000);
+            }
+        });
+    }
+
+    private void calculateAverages() {
+        // Thread to average the dB every 30 seconds
+        final Handler averageHandler = new Handler();
+        averageHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                double average = 0;
+                int count = 0;
+                for(Double decibel : decibelMeasures) {
+                    average += decibel;
+                    count += 1;
+                }
+                average = (average / count);
+                averageDecibelMeasures.add(average);
+                decibelMeasures.removeAll(decibelMeasures); // Wipe out the old measures to conserve memory
+                averageHandler.postDelayed(this, 30000);
+            }
+        });
+    }
+
+    private void sendData() {
+        // Thread to send the data to the server
+        final Handler dataHandler = new Handler();
+        dataHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Sending shit");
+                SendData dataSender = new SendData();
+                LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+                try {
+                    Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    double longitude = location.getLongitude();
+                    double latitude = location.getLatitude();
+                    if(averageDecibelMeasures.size() > 0) {
+                        for (Double decibel : averageDecibelMeasures) {
+                            try {
+                                dataSender.send(getApplicationContext(), latitude, longitude, decibel);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        averageDecibelMeasures.clear(); // Clear up averages so we don't have duplicate data entries
+                    }
+                } catch(final SecurityException e) {
+                    e.printStackTrace();
+                }
+                //dataHandler.postDelayed(this, 300000);
+                dataHandler.postDelayed(this, 60000);
+            }
+        });
+    }
+
     private void checkPermissions() {
         int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
+        int MY_PERMISSIONS_REQUEST_INTERNET = 2;
+        int MY_PERMISSIONS_REQUEST_LOCATION = 3;
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_CONTACTS)) {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECORD_AUDIO)) {
                 // Show why we need the damn permission
             } else {
                 // No explanation needed, we can request the permission.
                 ActivityCompat.requestPermissions(this,
                         new String[]{Manifest.permission.RECORD_AUDIO},
                         MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+            }
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.INTERNET)) {
+                // Show why we need the damn permission
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.INTERNET},
+                        MY_PERMISSIONS_REQUEST_INTERNET);
+            }
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Show why we need the damn permission
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
             }
         }
     }
---
+++ NoiseReporter/app/src/main/java/com/nohowdezign/noisereporter/network/SendData.java
@@ -0,0 +1,51 @@
+package com.nohowdezign.noisereporter.network;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+import com.nohowdezign.noisereporter.NoiseMap;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+
+import cz.msebera.android.httpclient.Header;
+import cz.msebera.android.httpclient.HttpEntity;
+import cz.msebera.android.httpclient.entity.StringEntity;
+
+public class SendData {
+    //private String url = "http://thefortress.xyz:7070/";
+    private String url = "http://noisereporter:7070/";
+
+    public void send(Context context, double lat, double lon, double decibelLevel) throws JSONException {
+        AsyncHttpClient client = new AsyncHttpClient();
+        HttpEntity entity;
+
+        JSONObject jsonData = new JSONObject();
+        jsonData.put("lat", lat);
+        jsonData.put("long", lon);
+        jsonData.put("decibelLevel", decibelLevel);
+        Log.d("HTTP", String.valueOf(decibelLevel));
+
+        entity = new StringEntity(jsonData.toString(), "UTF-8");
+        String  contentType = "application/json";
+
+        Log.d("HTTP", "Post...");
+        client.post(context, url + "report/", entity, contentType, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                Log.d("HTTP", "Post success");
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+            }
+        });
+    }
+
+}
