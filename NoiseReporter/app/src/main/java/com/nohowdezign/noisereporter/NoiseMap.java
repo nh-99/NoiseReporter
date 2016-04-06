19ea0df2acc536e33bd4f48992d9fb5452263f8c
nh-99 <ifence9@gmail.com>
2016-04-05 22:54:09 -0400

    Refactor & add in new activity for submitting noise reports

--- NoiseReporter/app/src/main/java/com/nohowdezign/noisereporter/NoiseMap.java
+++ NoiseReporter/app/src/main/java/com/nohowdezign/noisereporter/NoiseMap.java
@@ -1,22 +1,18 @@
 package com.nohowdezign.noisereporter;
 
 import android.Manifest;
-import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
-import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -26,32 +22,20 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.nohowdezign.noisereporter.audio.SoundMeter;
-import com.nohowdezign.noisereporter.network.SendData;
-
-import org.json.JSONException;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class NoiseMap extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private SoundMeter soundMeter;
-    private List<Double> decibelMeasures = new ArrayList<Double>();
-    private List<Double> averageDecibelMeasures = new ArrayList<Double>();
     private ListView mDrawerList;
     private ArrayAdapter<String> mAdapter;
     private ActionBarDrawerToggle mDrawerToggle;
     private DrawerLayout mDrawerLayout;
-    private String mActivityTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         checkPermissions();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_noise_map);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -59,18 +43,67 @@ public class NoiseMap extends AppCompatActivity implements OnMapReadyCallback {
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
         addDrawerItems();
         setupDrawer();
+    }
 
-        soundMeter = new SoundMeter();
-        soundMeter.start();
-        storeDecibels();
-        calculateAverages();
-        sendData();
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        // TODO: Poll server for noisy locations & drop pins
+        LatLng belfast = new LatLng(44.4229874, -69.0111177);
+        mMap.addMarker(new MarkerOptions().position(belfast).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(belfast));
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(14));
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return mDrawerToggle.onOptionsItemSelected(item)||super.onOptionsItemSelected(item);
+    }
+
+    private void checkPermissions() {
+        int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
+        int MY_PERMISSIONS_REQUEST_INTERNET = 2;
+        int MY_PERMISSIONS_REQUEST_LOCATION = 3;
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECORD_AUDIO)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.RECORD_AUDIO},
+                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+            }
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.INTERNET)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.INTERNET},
+                        MY_PERMISSIONS_REQUEST_INTERNET);
+            }
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+        }
     }
 
     private void setupDrawer() {
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mActivityTitle = getTitle().toString();
-
         mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
                 R.string.drawer_open, R.string.drawer_close) {
 
@@ -90,150 +123,18 @@ public class NoiseMap extends AppCompatActivity implements OnMapReadyCallback {
     }
 
     private void addDrawerItems() {
-        String[] osArray = { "Android", "iOS", "Windows", "OS X", "Linux" };
+        String[] osArray = {"Send Report"};
         mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, osArray);
         mDrawerList.setAdapter(mAdapter);
-    }
-
-    /**
-     * Drops pins on maps when it's ready
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        // TODO: Poll server for noisy locations & drop pins
-        LatLng belfast = new LatLng(44.4229874, -69.0111177);
-        mMap.addMarker(new MarkerOptions().position(belfast).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(belfast));
-        mMap.moveCamera(CameraUpdateFactory.zoomTo(14));
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        // Sync the toggle state after onRestoreInstanceState has occurred.
-        mDrawerToggle.syncState();
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        mDrawerToggle.onConfigurationChanged(newConfig);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Pass the event to ActionBarDrawerToggle, if it returns
-        // true, then it has handled the app icon touch event
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    private void storeDecibels() {
-        // Thread to get the dB
-        final Handler decibelHandler = new Handler();
-        decibelHandler.post(new Runnable() {
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void run() {
-                decibelMeasures.add(soundMeter.getDb());
-                decibelHandler.postDelayed(this, 5000);
-            }
-        });
-    }
-
-    private void calculateAverages() {
-        // Thread to average the dB every 30 seconds
-        final Handler averageHandler = new Handler();
-        averageHandler.post(new Runnable() {
-            @Override
-            public void run() {
-                double average = 0;
-                int count = 0;
-                for(Double decibel : decibelMeasures) {
-                    average += decibel;
-                    count += 1;
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        Intent myIntent = new Intent(getApplicationContext(), CreateReport.class);
+                        startActivity(myIntent);
                 }
-                average = (average / count);
-                averageDecibelMeasures.add(average);
-                decibelMeasures.removeAll(decibelMeasures); // Wipe out the old measures to conserve memory
-                averageHandler.postDelayed(this, 30000);
             }
         });
-    }
-
-    private void sendData() {
-        // Thread to send the data to the server
-        final Handler dataHandler = new Handler();
-        dataHandler.post(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Sending shit");
-                SendData dataSender = new SendData();
-                LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-                try {
-                    double latitude = 0;
-                    double longitude = 0;
-                    Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                    try {
-                        longitude = location.getLongitude();
-                        latitude = location.getLatitude();
-                    } catch(NullPointerException e) {
-                        e.printStackTrace();
-                    }
-                    if(averageDecibelMeasures.size() > 0) {
-                        for (Double decibel : averageDecibelMeasures) {
-                            try {
-                                dataSender.send(getApplicationContext(), latitude, longitude, decibel);
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                        averageDecibelMeasures.clear(); // Clear up averages so we don't have duplicate data entries
-                    }
-                } catch(final SecurityException e) {
-                    e.printStackTrace();
-                }
-                //dataHandler.postDelayed(this, 300000);
-                dataHandler.postDelayed(this, 60000);
-            }
-        });
-    }
-
-    private void checkPermissions() {
-        int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
-        int MY_PERMISSIONS_REQUEST_INTERNET = 2;
-        int MY_PERMISSIONS_REQUEST_LOCATION = 3;
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECORD_AUDIO)) {
-                // Show why we need the damn permission
-            } else {
-                // No explanation needed, we can request the permission.
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.RECORD_AUDIO},
-                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
-            }
-        }
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
-            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.INTERNET)) {
-                // Show why we need the damn permission
-            } else {
-                // No explanation needed, we can request the permission.
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.INTERNET},
-                        MY_PERMISSIONS_REQUEST_INTERNET);
-            }
-        }
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
-                // Show why we need the damn permission
-            } else {
-                // No explanation needed, we can request the permission.
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                        MY_PERMISSIONS_REQUEST_LOCATION);
-            }
-        }
     }
 }
---
+++ NoiseReporter/app/src/main/java/com/nohowdezign/noisereporter/report/ReportGenerator.java
@@ -0,0 +1,100 @@
+package com.nohowdezign.noisereporter.report;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Handler;
+
+import com.nohowdezign.noisereporter.NoiseMap;
+import com.nohowdezign.noisereporter.audio.SoundMeter;
+import com.nohowdezign.noisereporter.network.SendData;
+
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReportGenerator {
+    private SoundMeter soundMeter;
+    private List<Double> decibelMeasures = new ArrayList<Double>();
+    private List<Double> averageDecibelMeasures = new ArrayList<Double>();
+    private NoiseMap noiseMap;
+
+    public ReportGenerator(NoiseMap map) {
+        this.noiseMap = map;
+    }
+
+    public void generateReport() {
+        soundMeter = new SoundMeter();
+        soundMeter.start();
+        storeDecibels();
+        calculateAverages();
+        sendData();
+    }
+
+    private void storeDecibels() {
+        // Thread to get the dB
+        final Handler decibelHandler = new Handler();
+        decibelHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                decibelMeasures.add(soundMeter.getDb());
+                decibelHandler.postDelayed(this, 5000);
+            }
+        });
+    }
+
+    private void calculateAverages() {
+        // Thread to average the dB every 30 seconds
+        final Handler averageHandler = new Handler();
+        averageHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                double average = 0;
+                int count = 0;
+                for(Double decibel : decibelMeasures) {
+                    average += decibel;
+                    count += 1;
+                }
+                average = (average / count);
+                averageDecibelMeasures.add(average);
+                decibelMeasures.removeAll(decibelMeasures); // Wipe out the old measures to conserve memory
+                averageHandler.postDelayed(this, 30000);
+            }
+        });
+    }
+
+    private void sendData() {
+        // Thread to send the data to the server
+        final Handler dataHandler = new Handler();
+        dataHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Sending shit");
+                SendData dataSender = new SendData();
+                if (averageDecibelMeasures.size() > 0) {
+                    for (Double decibel : averageDecibelMeasures) {
+                        try {
+                            dataSender.send(noiseMap.getApplicationContext(), getLocation().getLatitude(), getLocation().getLongitude(), decibel);
+                        } catch (JSONException|NullPointerException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    averageDecibelMeasures.clear(); // Clear up averages so we don't have duplicate data entries
+                }
+                dataHandler.postDelayed(this, 60000);
+            }
+        });
+    }
+
+    private Location getLocation() {
+        LocationManager lm = (LocationManager) noiseMap.getSystemService(Context.LOCATION_SERVICE);try {
+            Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            return location;
+        } catch (final SecurityException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
