1bab4030861a10ee410e0e0c9125adfbaff679fa
nh-99 <ifence9@gmail.com>
2016-04-08 10:19:00 -0400

    Fixes for noise report UI

--- NoiseReporter/app/src/main/java/com/nohowdezign/noisereporter/fragments/ReportNoise.java
+++ NoiseReporter/app/src/main/java/com/nohowdezign/noisereporter/fragments/ReportNoise.java
@@ -3,19 +3,27 @@ package com.nohowdezign.noisereporter.fragments;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.nohowdezign.noisereporter.R;
 import com.nohowdezign.noisereporter.activities.NoiseMap;
 import com.nohowdezign.noisereporter.audio.SoundMeter;
+import com.nohowdezign.noisereporter.report.ReportGenerator;
 
 public class ReportNoise extends Fragment {
     private SoundMeter soundMeter;
     private OnFragmentInteractionListener mListener;
     private NoiseMap noiseMap;
+    private ReportGenerator reportGenerator;
+
     public ReportNoise() {}
 
     public static ReportNoise newInstance(String param1, String param2) {
@@ -26,6 +34,20 @@ public class ReportNoise extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        final ReportGenerator reportGenerator = new ReportGenerator(getActivity());
+        this.reportGenerator = reportGenerator;
+        reportGenerator.init();
+        soundMeter = reportGenerator.getSoundMeter();
+
+        final Handler decibelHandler = new Handler();
+        decibelHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                TextView decibelCounter = (TextView) getActivity().findViewById(R.id.decibelCounter);
+                decibelCounter.setText("Decibels: " + reportGenerator.getDecibels());
+                decibelHandler.postDelayed(this, 1000);
+            }
+        });
     }
 
     @Override
@@ -39,8 +61,17 @@ public class ReportNoise extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_report_noise, container, false);
+        RelativeLayout layout = (RelativeLayout) inflater.inflate(R.layout.fragment_report_noise, container, false);
+
+        FloatingActionButton fab = (FloatingActionButton) layout.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                reportGenerator.generateReport(view);
+            }
+        });
+
+        return layout;
     }
 
     public void onButtonPressed(Uri uri) {
